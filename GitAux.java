c8c948d269c4d140b8f79c098d37fd45fc830b41
Mauryicvc <42949555+Mauryicvc@users.noreply.github.com>
2020-09-16 11:29:58 +0200

    Create GitAux.java

---
+++ GitAux.java
@@ -0,0 +1,9 @@
+public class GitAux {
+    public void metodoAlumno1(){
+        System.out.println(*metodo 1*);
+    }
+    public void metodoComunitario(){
+        // comentario incluido para pruebas
+        System.out.println("Aqui escribimos todos");
+    }
+  }
