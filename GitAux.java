03d01df07ed498531656d3b5e3336c2c71273d5a
jlopezca <jesusalejandro.lopez@orange.com>
2020-09-16 09:35:20 +0000

    modif alumno 2

--- GitAux.java
+++ GitAux.java
@@ -2,8 +2,13 @@ public class GitAux {
     public void metodoAlumno1(){
         System.out.println(*metodo 1*);
     }
+    public void metodoAlumno2(){
+        System.out.println("metodo 2");
+    }
+     
     public void metodoComunitario(){
         // comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
+        System.out.println("aluno 2"); // modif por alumno 2
     }
   }
